f55d430cbc15e9deda96f17543522f47ac35b7b1
toby cabot <toby@caboteria.org>
2024-05-02 12:54:48 -0400

    Fix a test that used the wrong YAML manifest
    The GatewaySecretReferenceGrantAllInNamespace test used the
    GatewaySecretReferenceGrantSpecific test's manifest.
    
    This commit also changes the name of this test's Gateway so it's
    distinct from the other tests' Gateways. This helps prevent
    eventual-consistency problems with the test suite. If tests share
    resources then one test can delete the shared resource, but when the
    next test runs the cluster hasn't yet processed the delete and the
    test setup code gets confused.

--- conformance/tests/gateway-secret-reference-grant-all-in-namespace.go
+++ conformance/tests/gateway-secret-reference-grant-all-in-namespace.go
@@ -37,7 +37,7 @@ var GatewaySecretReferenceGrantAllInNamespace = suite.ConformanceTest{
 	Features:    []suite.SupportedFeature{suite.SupportReferenceGrant},
 	Manifests:   []string{"tests/gateway-secret-reference-grant-all-in-namespace.yaml"},
 	Test: func(t *testing.T, s *suite.ConformanceTestSuite) {
-		gwNN := types.NamespacedName{Name: "gateway-secret-reference-grant", Namespace: "gateway-conformance-infra"}
+		gwNN := types.NamespacedName{Name: "gateway-secret-reference-grant-all", Namespace: "gateway-conformance-infra"}
 
 		t.Run("Gateway listener should have a true ResolvedRefs condition and a true Programmed condition", func(t *testing.T) {
 			listeners := []v1beta1.ListenerStatus{{
